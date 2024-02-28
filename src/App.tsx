662f0cd19813b5dc8001fd5ec2887b465fe1cb52
Yann D'Souza <yannssouza@outlook.com>
2024-02-28 09:29:10 -0300

    CRUD Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/home/Home";
+
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             {/*<Route path="/login" element={<Login />} />*/}
+            {/*<Route path="/cadastro" element={<Cadastro />} />*/}
             <Route path="/home" element={<Home />} />
           </Routes>
         </div>
