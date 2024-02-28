2b2a46610d5a3ba1aefbc567d35b9f92e9a0849c
Yann D'Souza <yannssouza@outlook.com>
2024-02-28 09:01:52 -0300

    Projeto iniciado

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="w-full bg-red-500 text-white flex justify-center py-4">
+        <div className="container flex justify-between text-lg">
+          <div className="text-2xl font-bold uppercase">Farmacia</div>
+
+          <div className="flex gap-4">
+            <div className="hover:underline">Produtos</div>
+            <div className="hover:underline">Categorias</div>
+            <div className="hover:underline">Cadastrar Categoria</div>
+            <div className="hover:underline">Perfil</div>
+            <div className="hover:underline">Sair</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
