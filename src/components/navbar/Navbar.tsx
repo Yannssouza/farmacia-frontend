275f172a8bc0ddc472a117179e68c9290d4ae187
Yann D'Souza <yannssouza@outlook.com>
2024-02-28 09:30:17 -0300

    CRUD Configuration

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,16 +9,16 @@ function Navbar() {
           <div className="text-2xl font-bold uppercase">Farmacia</div>
 
           <div className="flex gap-4">
-            <Link to="/login" className="hover:underline">
-              Login
-            </Link>
-            <Link to="/home" className="hover:underline">
-              Home
-            </Link>
+            {/*<Link to="/login" className="hover:underline">*/}
+            {/*  Login*/}
+            {/*</Link>*/}
+            {/*<Link to="/home" className="hover:underline">*/}
+            {/*  Home*/}
+            {/*</Link>*/}
             <div className="hover:underline">Produtos</div>
             <div className="hover:underline">Categorias</div>
             <div className="hover:underline">Cadastrar Categoria</div>
-            <div className="hover:underline">Perfil</div>
+            {/*<div className="hover:underline">Perfil</div>*/}
             <div className="hover:underline">Sair</div>
           </div>
         </div>
