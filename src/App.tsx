3e14b8ee42b44e07b53004d26407dd00baf0970e
Yann D'Souza <yannssouza@outlook.com>
2024-02-28 09:19:55 -0300

    CRUD Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <div className="min-h-[80vh]">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            {/*<Route path="/login" element={<Login />} />*/}
             <Route path="/home" element={<Home />} />
           </Routes>
         </div>
